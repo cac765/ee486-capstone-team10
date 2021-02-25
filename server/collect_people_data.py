72c1a649178b71a091578ca3d3a35d29fc9db25e
saralynnbc <43869161+saralynnbc@users.noreply.github.com>
2021-02-24 18:09:21 -0700

    Update collect_people_data.py
    added comments

--- server/collect_people_data.py
+++ server/collect_people_data.py
@@ -1,1 +1,6 @@
-# this is fun
+#Add MQTT publish UPDATE command
+#publish "UPDATE" to MQTT topic "eecapstone/snapshot"
+#Add MQTT subscribe loop to collet data from all cameras
+#Check message list length against number of cameras
+#if message length == number  of cameras, break
+
