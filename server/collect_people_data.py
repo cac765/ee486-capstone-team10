2a8e0e841d749b6f39f17d0d8cfe82d11e87f58e
saralynnbc <slb672@nau.edu>
2021-02-25 21:18:22 -0700

    first updates to collect_people_data

--- server/collect_people_data.py
+++ server/collect_people_data.py
@@ -4,64 +4,44 @@
 #Check message list length against number of cameras
 #if message length == number  of cameras, break
 
- 
-if broker_ip:
-    edge = MQTTClient( broker_ip, client_name )
-    edge.client.on_message = on_message
-    edge.connect()
-    edge.loop_start()
-    edge.subscribe( "eecapstone/snapshot" )
-    
-    recording = True 
-    
-while recording:
-    if broker_ip:
-        # Wait infinitely for the UPDATE command to be received from the broker
-        cmd_received = False
-        while not cmd_received:
+# Define callback function for MQTT message callback
+def on_message( client, userdata, message ):
+    """Callback function for receiving messages."""
+    msg = message.payload.decode( "utf-8" )
+    store.msg_queue.append( msg )
+    logging.info( "\n\tTopic: {}\n\tMessage: {}\n\tRetained: {}".format(
+                  message.topic,msg, message.retain ) )
+
+    if ( message.retain == 1 ):
+        logging.info( "This was a retained message." )
+
+def main():
+
+    store = MQTTClient( broker_ip, client_name )
+    #sets to run callback function
+    store.client.on_message = on_message
+    #connecting to broker
+    store.connect()
+    #starts a thread for MQTT messages
+    store.loop_start()
+    #subscribe to topic, qos-quality of service: double checking, retain- forget about it 
+    store.publish( "eecapstone/snapshot", "UPDATE", qos = 2, retain = False )
+    #subscribe and collect data from all cameras
+    store.subscribe( "test/occupancy")
+
+    data_received = False
+
+    while not data_received:
             # Check if the message queue has messages
-            if len( edge.msg_queue ) > 0:
+            if len( store.msg_queue ) > 1:
                 # Grab the message from the queue
-                incoming_msg = edge.msg_queue.pop()
-                # Check if message was the UPDATE command, exit from the loop
-                if incoming_msg == "UPDATE":
-                    cmd_received = True
-                    
-        # Load the image from the stream and reset the command flag
-        frame1 = camerastream.read()
-        cmd_received = False
+                incoming_data1 = store.msg_queue.pop()
+                incoming_data2 = store.msg_queue.pop()
+
+                data_received = True
         
-   # If no broker specified, take the snapshot now and continue
-    else:
-        frame1 = camerastream.read()
-
-    # Acquire frame and resize to expected shape [1xHxWx3]
-    frame = frame1.copy()
-    #frame = cv2.flip( frame, -1 ) # Uncomment to flip image from camera stream
-    frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-    frame_resized = cv2.resize(frame_rgb, (width, height))
-    input_data = np.expand_dims(frame_resized, axis=0)
-
-    # Normalize pixel values if using a floating model (i.e. if model is non-quantized)
-    if floating_model:
-        input_data = (np.float32(input_data) - input_mean) / input_std
-
-    # Perform the actual detection by running the model with the image as input
-    interpreter.set_tensor(input_details[0]['index'],input_data)
-    interpreter.invoke()
-
-    # Retrieve detection results
-    boxes = interpreter.get_tensor(output_details[0]['index'])[0] # Bounding box coordinates of detected objects
-    classes = interpreter.get_tensor(output_details[1]['index'])[0] # Class index of detected objects
-    scores = interpreter.get_tensor(output_details[2]['index'])[0] # Confidence of detected objects
-    #num = interpreter.get_tensor(output_details[3]['index'])[0]  # Total number of detected objects (inaccurate and not needed)
-
-    # Track number of occupants/locations
-    num_occupants = 0
-    locations = []
-    
-    
-    #this is where we compare the camera count to the message list count, do we want to enable log through mosquitto to see who is subscribed? 
+if __name__ == "__main__":
+    main()
     
     
     
