c1eb42466bc6075a4e880a5d3a6c123b96a384be
cac765 <cac765@nau.edu>
2021-03-14 15:34:37 -0600

    Changed default conf threshold to 60%

--- image_people_detector.py
+++ image_people_detector.py
@@ -51,7 +51,7 @@ parser.add_argument('--labels', help='Name of the labelmap file, if different \
                     default='labelmap.txt')
 parser.add_argument('--threshold', help='Minimum confidence threshold for \
                                          displaying detected objects',
-                    default=0.5)
+                    default=0.6)
 parser.add_argument('--resolution', help='Desired webcam resolution in WxH. \
                                         If the webcam does not support the \
                                         resolution entered, errors may occur.',
